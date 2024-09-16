188f7811658ce61539dd6766f50224ba93714b83
Do-Thi-Thom <dothithom.5894.gkfxprime@gmail.com>
2024-09-16 12:00:08 +0700

    HW_ss08_Thom

---
+++ bt08.js
@@ -0,0 +1,93 @@
+// EX 01-------------------------------------------------------------
+// alert("Hi!!!");
+// console.log("Chào");
+
+// EX 02--------------------------------------------------------
+// let i = 10;
+// console.log("i (số nguyên):",i);
+
+// let f = 20.5;
+// console.log("f (số thực):",f);
+
+// let b = true;
+// console.log("b (logic):",b);
+
+// let s = "Hà Nội";
+// console.log("s (chuỗi):",s);
+
+// EX 03----------------------------------------------------------------
+// let width = 10;
+// let height = 20;
+// let acrea = width * height;
+// document.write("Diện tích của màn hình chữ nhật là:" + area);
+
+// EX 04---------------------------------------------------------------
+// alert("Bạn đủ 18 tuổi chưa?");
+
+// EX 05----------------------------------------------------------------
+// let physics = +prompt("Nhập điểm Vật Lý");
+// let chemistry = +prompt("Nhập điểm Hóa Học");
+// let biologi = +prompt("Nhập điểm Sinh Học");
+
+// let total = physics + chemistry + biologi;
+// let average = total/3;
+
+// console.log("Tổng điểm:" + total);
+// console.log("Điểm trung bình:" + average);
+
+// EX 06-----------------------------------------------------------------
+// em có làm nhưng không hiểu 
+
+
+
+// EX 07-----------------------------------------------------------------
+// let width = +prompt("Nhập chiều rộng của hình chữ nhật");
+// let height = +prompt("Nhập chiều dài của hình chữ nhật");
+// let primeter = 2 * (width + height);
+// let acrea = width * height;
+// console.log("Chu vi hình chữ nhật là:",primeter);
+// console.log("Diện tích của hình chữ nhật:",acrea);
+
+// EX 08---------------------------------------------------------------------
+// const pi = 3.14;
+// let radius = +prompt("Nhập bán kính của hình tròn");
+// let primeter = radius * pi;
+// let acrea = radius * radius * pi;
+// console.log("Chu vi hình tròn là:",primeter);
+// console.log("Diện tích hình tròn là:",acrea);
+
+// EX 09----------------------------------------------------------------------
+// tóan tử == là toán tử so sánh về giá trị 
+// toán tử === là so sánh tuyệt đối về giá trị và kiểu giữ liệu.
+
+// let a = 5;
+// kiểu dữ liệu number
+// let b = "5";
+// kiểu dữ kiệu string
+
+// console.log(a == b);
+// a == b so sánh giá trị = nhau >true
+// console.log(a === b);
+// a === b so tuyệt đối về giá trị và kiểu dữ liệu > false
+// console.log(a!=b);
+// a khác b so sánh về giá trị > false
+// console.log(a!==b);
+// a khác b so sánh về giá trị và kiểu giữ liệu >true
+
+// EX 10---------------------------------------------------------------------
+let a = 6;
+let result = ++a + a + a++ + ++a + a;
+console.log(result); 
+// kết quả ra 39 em đã tìm hiểu nhưng e vẫn không hiểu
+
+
+
+
+
+
+
+
+
+
+
+
